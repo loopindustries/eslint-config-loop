5c30d915f6b5b0c8854306f906dbb9d2857f8be2
Matthew Weltman <matthew.weltman@loopindustries.se>
2018-04-20 13:22:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+moodule.exports = {
+  parser: "babel-eslint",
+  extends: "airbnb",
+  plugins: ["react", "jsx-a11y", "import"],
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    semi: [2, "never"],
+  },
+  env: {
+    jest: true
+  }
+}
