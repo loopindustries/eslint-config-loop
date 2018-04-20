3fc7b3d30c5c2dd7b6a843b524e51c488be2ba0f
Di <dichai1983@gmail.com>
2018-04-20 14:50:44 +0200

    Turn of no-unused-expressions

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ module.exports = {
   extends: "airbnb",
   plugins: ["react", "jsx-a11y", "import"],
   rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     semi: [2, "never"],
+    "no-unused-expressions": "off"
   },
   env: {
     jest: true,
     browser: true
   }
-}
+};
