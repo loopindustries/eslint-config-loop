30bce0ba2b6cc3d4f4946bf33f1332ae929d07c3
Di <dichai1983@gmail.com>
2018-04-26 13:52:23 +0200

    Disable jsx-a11y/no-noninteractive-element-interaction

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ module.exports = {
     "jsx-a11y/anchor-is-valid": 0,
     "jsx-a11y/click-events-have-key-events": 0,
     "jsx-a11y/label-has-for": 0,
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
     "jsx-a11y/no-autofocus": 0,
+    "no-bitwise": 0,
+    "no-mixed-operators": 0,
     "no-restricted-syntax": 0,
     "no-underscore-dangle": 0,
     "no-unused-expressions": 0,
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/jsx-indent": 0,
-    "no-bitwise": 0,
     "react/prop-types": 0,
-    "no-mixed-operators": 0,
     semi: [2, "never"]
   },
   env: {
